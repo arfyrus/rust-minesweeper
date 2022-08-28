b1e1c14fe5751fc1eedc50c01b77d4eb84110a8a
Arfi <104511481+arfyrus@users.noreply.github.com>
2022-08-28 14:42:28 +0800

    Added Cell::flag()

--- src/cell.rs
+++ src/cell.rs
@@ -9,35 +9,26 @@ pub struct Cell {
     pub ctype: CellType,
 }
 
-impl Cell {}
+impl Cell {
+    pub fn reveal(&mut self) -> bool {
+        if self.reveal {
+            return false;
+        }
+        self.reveal = true;
 
-pub fn reveal(cell: &mut Cell /*, x: i32, y: i32, grid: &mut Vec<Cell>*/) -> bool {
-    if let CellType::Bomb = cell.ctype {
-        return true;
-    }
-    if cell.reveal {
+        if let CellType::Bomb = self.ctype {
+            return true;
+        }
         return false;
     }
-    cell.reveal = true;
-    // if let CellType::Field(num) = cell.ctype {
-    //     if num > 0 {
-    //         return false;
-    //     }
-    // }
-    // for y2 in -1..=1 {
-    //     for x2 in -1..=1 {
-    //         if x + x2 >= 0 && y + y2 >= 0 && x + x2 < super::header::W && x + y2 < super::header::H {
-    //             let mut grid2 = &mut *grid;
-    //             reveal(
-    //                 &mut grid2[super::header::index(x + x2, y + y2)],
-    //                 x + x2,
-    //                 y + y2,
-    //                 &mut grid2
-    //             );
-    //         }
-    //     }
-    // }
-    return false;
+
+    pub fn flag(&mut self) -> bool {
+        self.flag = true;
+        if let CellType::Bomb = self.ctype {
+            return true;
+        }
+        return false;
+    }
 }
 
 pub fn new(bomb: bool) -> Cell {
