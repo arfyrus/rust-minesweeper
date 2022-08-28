17dd0db3a093258082c7e882726c24f1751024c9
Arfi <104511481+arfyrus@users.noreply.github.com>
2022-08-28 18:03:41 +0800

    Added 'Features to add' in README

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
 pub const W: i32 = 15;
 pub const H: i32 = 15;
-pub const DISPLAY_SPACE: usize = 5;
-pub const BOMB_PERCENT: i32 = 75;
+pub const DISPLAY_SPACE: usize = 7;
+pub const BOMB_PERCENT: i32 = 85;
